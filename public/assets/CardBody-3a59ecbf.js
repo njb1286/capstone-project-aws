08b702f49526e078e566c1895e141a920bda32c9
Njb1286 <bennionnj@gmail.com>
2024-01-26 12:55:46 -0700

    Updated the way search validation is handled, and added server run checking

---
+++ public/assets/CardBody-3a59ecbf.js
@@ -0,0 +1,1 @@
+import{r as e,u as c,j as m,c as p}from"./index-6c100cd4.js";const s=e.forwardRef(({className:r,bsPrefix:a,as:o="div",...d},t)=>(a=c(a,"card-body"),m.jsx(o,{ref:t,className:p(r,a),...d})));s.displayName="CardBody";const n=s;export{n as C};
